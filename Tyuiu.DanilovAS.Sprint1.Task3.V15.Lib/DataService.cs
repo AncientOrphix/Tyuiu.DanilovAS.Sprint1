de56710cdb0a21fb16a3ff17e715a856c5f4f315
Tyuiu.DanilovAlexander <Tyuiu.DanilovAlexander@outlook.com>
2024-09-21 21:20:27 +0500

    Task3

--- Tyuiu.DanilovAS.Sprint1.Task3.V15.Lib/DataService.cs
+++ Tyuiu.DanilovAS.Sprint1.Task3.V15.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.DanilovAS.Sprint1.Task3.V15.Lib
         public double DistanceOverTime(double v1, double v2, double S, double T)
         {
 
-            return Math.Round(Math.Abs(T*v1-T*v2+S),3);
+            return Math.Round((v1 + v2) * T + S, 3);
         }
     }
 }
